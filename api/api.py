02c18acfe37070879abe06b87318776334e27257
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-11-09 21:01:41 +0000

    Wire up on_insert_articles

--- api/api.py
+++ api/api.py
@@ -1,6 +1,11 @@
 from os import environ
 from eve import Eve
 
+from resources import before_insert_articles
+
+app = Eve()
+app.on_insert_articles += before_insert_articles
+
 if __name__ == '__main__':
     # Heroku support: bind to PORT if defined, otherwise default to 5000.
     if 'PORT' in environ:
@@ -9,5 +14,4 @@ if __name__ == '__main__':
     else:
         port = 5000
         host = '127.0.0.1'
-    app = Eve()
     app.run(host=host, port=port)
