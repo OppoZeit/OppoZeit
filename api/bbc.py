775d092c4269daf41ab4bb13558146609eb128e4
Thomas Lim <txl11@ic.ac.uk>
2013-11-09 13:44:31 +0000

    Now have the barebones of the BBC Juicer API in place

---
+++ api/bbc.py
@@ -0,0 +1,26 @@
+from restkit import Resource
+import os
+
+try:
+    import simplejson as json
+except ImportError:
+    import json  # py2.6 only
+
+
+class Juicer(Resource):
+
+    def __init__(self, **kwargs):
+        base_url = 'http://bbc.api.mashery.com/juicer-ld-api'
+        try:
+            self.api_key = os.environ['bbc_api_key']
+        except KeyError:
+            exit('No BBC Api Key provided')
+        super(Juicer, self).__init__(base_url, follow_redirect=True,
+                                     max_follow_redirect=10, **kwargs)
+
+    def request(self, *args, **kwargs):
+        kwargs['api_key'] = self.api_key
+        resp = super(Juicer, self).request('get', **kwargs)
+        return json.loads(resp.body_string())
+
+bbc = Juicer()
