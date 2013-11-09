fa52911656c08b4e8669c5496e748bfb1013e892
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-11-09 20:59:20 +0000

    Use cps_id as the _id for articles

--- api/resources.py
+++ api/resources.py
@@ -2,6 +2,12 @@
 
 from schemas import article_schema
 
+
+def before_insert_articles(documents):
+    for d in documents:
+        # Copy cps_id to _id since we want to use it as unique identifier
+        d['_id'] = d['cps_id']
+
 article = {
     "item_title": "article",
     "schema": article_schema,
