1ace4ffaaf0b5143e09db0a23edde594a6971282
Thomas Lim <txl11@ic.ac.uk>
2013-11-10 14:30:56 +0000

    Have added key word extractor and getty image API in to resources

--- api/resources.py
+++ api/resources.py
@@ -6,12 +6,16 @@ from schemas import article_schema
 def before_insert_articles(documents):
     from sentiment import calculateSentiment, rip_body
     from twitter import findSemanticTweets
+    from getty import Getty
+    from extractor import keywords
+    g = Getty()
     for d in documents:
         # Copy cps_id to _id since we want to use it as unique identifier
         d['_id'] = d['cps_id']
         d['body_text'] = rip_body(d['url'])
         d['sentiment'] = calculateSentiment(d['url'], body=d['body_text'])
         d['related_tweets'] = findSemanticTweets(d['url'])
+        d['getty_images'] = g.findAndReturn(keywords(d['description']), 3)
 
 article = {
     "item_title": "article",
