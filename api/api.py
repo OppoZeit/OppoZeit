9b069a5410d8d680d8355464727b89f4dd06c718
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-11-10 02:18:05 +0000

    Add api functions update_if_exists and insert_or_update
    FIXME: Find a way to update existing articles, but Eve has a deep
    rooted assumption that _id is an ObjectId

--- api/api.py
+++ api/api.py
@@ -1,10 +1,25 @@
 from os import environ
 from eve import Eve
+from eve.methods.post import post
 
 from resources import before_insert_articles
 
 app = Eve()
 app.on_insert_articles += before_insert_articles
+
+
+def update_if_exists(articles):
+    with app.test_request_context():
+        # FIXME: Find a way to update existing articles, but Eve has a deep
+        # rooted assumption that _id is an ObjectId
+        existing = [article for article in articles
+                    if app.data.find_one('articles', cps_id=article['cps_id'])]
+    return [article for article in articles if not article in existing]
+
+
+def insert_or_update(articles):
+    with app.test_request_context():
+        return post('articles', update_if_exists(articles))
 
 if __name__ == '__main__':
     # Heroku support: bind to PORT if defined, otherwise default to 5000.
