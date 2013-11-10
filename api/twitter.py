c68a81f13f1cb4d546a90bea3715dd6f1565d9b8
Thomas Lim <txl11@ic.ac.uk>
2013-11-10 13:40:05 +0000

    Now saving twitter as per schema

--- api/twitter.py
+++ api/twitter.py
@@ -32,7 +32,25 @@ def findSemanticTweets(url):
                 opinion = opinion + 1
                 tweets.append(t)
 
+        for t in iterator.items():
+            # for t in tweets:
+            # print t.author.screen_name
+            exit
+            total = total + 1
+            blob = TextBlob(t.text)
+            if blob.sentiment[1] != 0:
+                d = {}
+                d['handle'] = t.author.screen_name
+                d['text'] = t.text
+                s = blob.sentiment
+                d['sentiment'] = {}
+                d['sentiment']['polarity'] = s[0]
+                d['sentiment']['strength'] = s[1]
+                print (t.text, blob.sentiment)
+                opinion = opinion + 1
+                tweets.append(d)
+
     return tweets
 
-
-findSemanticTweets('http://www.bbc.co.uk/news/uk-24877081')
+if __name__ == '__main__':
+    print findSemanticTweets('http://www.bbc.co.uk/news/uk-24877081')
