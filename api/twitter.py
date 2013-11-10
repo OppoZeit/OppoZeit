926f01f9967fa1d25bcfb4044a397566dd6ebef2
Thomas Lim <txl11@ic.ac.uk>
2013-11-10 13:52:22 +0000

    Update twitter so we only do one query per search

--- api/twitter.py
+++ api/twitter.py
@@ -19,36 +19,24 @@ def findSemanticTweets(url):
     opinion = 0
     tweets = []
 
-    while opinion < 15:
+    iterator = tweepy.Cursor(api.search, url, count=100)
 
-        iterator = tweepy.Cursor(api.search, url, count=100)
-
-        for t in iterator.items():
-            # for t in tweets:
-            total = total + 1
-            blob = TextBlob(t.text)
-            if blob.sentiment[1] != 0:
-                print (t.text, blob.sentiment)
-                opinion = opinion + 1
-                tweets.append(t)
-
-        for t in iterator.items():
-            # for t in tweets:
-            # print t.author.screen_name
-            exit
-            total = total + 1
-            blob = TextBlob(t.text)
-            if blob.sentiment[1] != 0:
-                d = {}
-                d['handle'] = t.author.screen_name
-                d['text'] = t.text
-                s = blob.sentiment
-                d['sentiment'] = {}
-                d['sentiment']['polarity'] = s[0]
-                d['sentiment']['strength'] = s[1]
-                print (t.text, blob.sentiment)
-                opinion = opinion + 1
-                tweets.append(d)
+    for t in iterator.items():
+        # for t in tweets:
+        # print t.author.screen_name
+        total = total + 1
+        blob = TextBlob(t.text)
+        if blob.sentiment[1] != 0:
+            d = {}
+            d['handle'] = t.author.screen_name
+            d['text'] = t.text
+            s = blob.sentiment
+            d['sentiment'] = {}
+            d['sentiment']['polarity'] = s[0]
+            d['sentiment']['strength'] = s[1]
+            print (t.text, blob.sentiment)
+            opinion = opinion + 1
+            tweets.append(d)
 
     return tweets
 
