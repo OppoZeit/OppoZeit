3bfa417d9fa760666d6eaf28760e57585d0dd6cf
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-11-10 13:47:22 +0000

    Add sentiment and body_text before saving article

--- api/resources.py
+++ api/resources.py
@@ -1,12 +1,15 @@
 """Resources."""
 
 from schemas import article_schema
+from sentiment import calculateSentiment, rip_body
 
 
 def before_insert_articles(documents):
     for d in documents:
         # Copy cps_id to _id since we want to use it as unique identifier
         d['_id'] = d['cps_id']
+        d['body_text'] = rip_body(d['url'])
+        d['sentiment'] = calculateSentiment(d['url'], body=d['body_text'])
 
 article = {
     "item_title": "article",
