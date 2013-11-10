d24fd091610546940d25562807d2f084c6f71d18
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-11-10 18:47:32 +0000

    Getty wants a comma-separated list

--- api/resources.py
+++ api/resources.py
@@ -14,7 +14,8 @@ def before_insert_articles(documents):
         d['_id'] = d['cps_id']
         d['sentiment'] = calculateSentiment(d['url'])
         d['related_tweets'] = findSemanticTweets(d['url'])
-        d['getty_images'] = g.findAndReturn(keywords(d['description']), 3)
+        d['getty_images'] = g.findAndReturn(
+            ', '.join(keywords(d['description'])), 3)
 
 article = {
     "item_title": "article",
