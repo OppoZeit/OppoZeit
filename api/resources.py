00714e6e5ffbfe9527acc927b0c31b72e39f3490
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-11-13 01:06:17 +0000

    API keys for Twitter and Getty have expired

--- api/resources.py
+++ api/resources.py
@@ -5,16 +5,10 @@ from schemas import article_schema
 
 def before_insert_articles(documents):
     from sentiment import calculateSentiment, rip_body
-    from twitter import findSemanticTweets
-    from getty import Getty
-    from extractor import keywords
-    g = Getty()
     for d in documents:
         d['body_text'] = rip_body(d['url'])
         d['sentiment'] = calculateSentiment(d['url'], body=d['body_text'])
-        d['related_tweets'] = findSemanticTweets(d['url'])
-        d['getty_images'] = g.findAndReturn(
-            ', '.join(keywords(d['description'])), 3)
+        # API keys for Twitter and Getty have expired
 
 article = {
     "item_title": "article",
